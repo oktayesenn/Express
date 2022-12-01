bda8e31f9a4fcd0103627349bcd99c55c9ad11ed
Oktay esen <oktayesenn@gmail.com>
2022-12-01 07:30:12 -0500

    Paths are created

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.get('/api', (req, res) => res.send("Buy, Rent"));
+app.get('/buy', (req, res) => res.send("Buy"));
+app.get("/rent", (req, res)=> res.send("Rent"));
+app.get('/about', (req, res) => res.send("About"));
+app.get('/contact', (req, res) => res.send("Contact"));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
