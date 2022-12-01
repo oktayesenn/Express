cbb0837f178c7ad19a30c0909203f18a73ee8c9e
Oktay esen <oktayesenn@gmail.com>
2022-11-30 19:32:54 -0500

    Express Created

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+app.get('/api', (req, res) => res.send("Buy, Rent"));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
